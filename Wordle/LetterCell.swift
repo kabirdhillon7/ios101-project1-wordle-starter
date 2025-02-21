57a1240bb1c10c32467ad6dcd04dc066001548fd
Kabir Dhillon <kabirdhillon99@gmail.com>
2025-02-20 23:10:44 -0800

    Objective 3: Adding more words

--- Wordle/LetterCell.swift
+++ Wordle/LetterCell.swift
@@ -50,7 +50,19 @@ class LetterCell: UICollectionViewCell {
      */
     func set(style: LetterCellStyle) {
         // START YOUR CODE HERE
-        // ...
+        switch style {
+        case .initial:
+            self.backgroundColor = .black
+            self.layer.borderColor = UIColor.gray.cgColor
+        case .incorrect:
+            self.backgroundColor = UIColor(red: 0.23, green: 0.23, blue: 0.24, alpha: 1.0)
+        case .correctLetterOnly:
+            self.backgroundColor = UIColor(red: 0.69, green: 0.63, blue: 0.30, alpha: 1.0)
+            self.layer.borderColor = UIColor(red: 0.69, green: 0.63, blue: 0.30, alpha: 1.0).cgColor
+        case .correctLetterAndPosition:
+            self.backgroundColor = UIColor(red: 0.38, green: 0.55, blue: 0.33, alpha: 1.0)
+            self.layer.borderColor = UIColor(red: 0.38, green: 0.55, blue: 0.33, alpha: 1.0).cgColor
+        }
         // END YOUR CODE HERE
     }
 }
